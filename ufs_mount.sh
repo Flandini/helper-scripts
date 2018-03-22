7ea0a1335257da6385bb6388ea0ed1f8771a3de3
Michael Flanders <flanders.michaelk@utexas.edu>
2018-03-22 18:39:08 -0500

    removed -r on mkdir

--- ufs_mount.sh
+++ ufs_mount.sh
@@ -6,6 +6,6 @@ list=`fdisk -l | grep UFS | cut -d '/' -f 3 | cut -d ' ' -f 1`
 
 for device in $list; do
 	rm -r $device 2>/dev/null
-	mkdir -r $device 2>/dev/null
+	mkdir $device 2>/dev/null
 	mount -r -t ufs -o ufstype=ufs2 /dev/$device $device 2>/dev/null
 done
